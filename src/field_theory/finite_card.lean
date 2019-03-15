d073e1565434bbb4574e531d32b085b2df2b7d81
Unknown <css_mn@yahoo.ie>
2019-03-15 11:17:11 +0100

    Improved  subfield_Xq_X

--- src/field_theory/finite_card.lean
+++ src/field_theory/finite_card.lean
@@ -29,40 +29,23 @@ let ⟨p, hc⟩ := char_p.exists α in ⟨p, @finite_field.card α _ _ p hc⟩
 
 open polynomial
 
+lemma root_Xq_X_iff (β : Type v) (x : β) [discrete_field β] {q : ℕ} :
+  x ∈ (↑(X^q - X : polynomial β).roots : set β) ↔ x^q = x :=
+have h0 : (X^q - X : polynomial β) ≠ 0, from sorry,
+calc x ∈ (↑(X^q - X : polynomial β).roots : set β)
+      ↔ is_root (X ^ q - X) x : by rw [finset.mem_coe, mem_roots h0]
+  ... ↔ -x + x ^ q = 0        : by simp --only [polynomial.eval_X,polynomial.eval_neg,iff_self,add_comm,polynomial.eval_pow,polynomial.eval_add,sub_eq_add_neg,polynomial.is_root.def]
+  ... ↔ x^q = x               : by rw[←add_left_inj x, add_zero, add_neg_cancel_left]
+
 --note: the characteristic of β follows from i : α → β
-theorem poly_subfield (p : ℕ) [char_p α p] (n : ℕ) (β : Type v) [discrete_field β] [char_p β p]
-  (i : α → β) [is_field_hom i] (h : splits i (X^(p^n) - X)) :
+theorem subfield_Xq_X (β : Type v) [discrete_field β] (p : ℕ) [char_p β p] (hp : nat.prime p) (n : ℕ) :
   is_subfield (↑(X^(p^n) - X : polynomial β).roots : set β) :=
   let f := (X^(p^n) - X : polynomial β) in
-  have h0 : f ≠ 0, from sorry,
-  have hp : nat.prime p, from char_p.is_prime α p,
-  { zero_mem := begin rw [finset.mem_coe, mem_roots], simp,
-      exact zero_pow (nat.pow_pos hp.pos n), exact h0 end,
-    one_mem := by rw [finset.mem_coe, mem_roots]; simp; exact h0,
-    add_mem := λ a b ha hb, begin rw [finset.mem_coe, mem_roots] at ha hb ⊢, simp at ha hb ⊢,
-      rw [add_pow_n_char β hp a b],
-      exact calc -a + (-b + (a^p^n + b^p^n))
-          = (-a + a^p^n) + (-b + b^p^n) : by simp only [neg_inj', add_comm, eq_self_iff_true, add_right_inj, add_left_comm]
-      ... = 0                           : by simp only [ha, hb, add_zero, eq_self_iff_true],
-      repeat {exact h0} end,
-    neg_mem := λ a ha, begin rw [finset.mem_coe, mem_roots] at ha ⊢, simp at ha ⊢,
-      exact calc a + -a ^ p ^ n = -(-a - -a^p^n) : by simp only [add_comm, eq_self_iff_true, neg_add_rev, add_right_inj, neg_neg, sub_eq_add_neg]
-                            ... = -(-a +  a^p^n) : sorry
-                            ... = 0              : by simp only [ha, eq_self_iff_true, neg_zero],
-      repeat {exact h0} end,
-    mul_mem := λ a b ha hb, begin rw [finset.mem_coe, mem_roots] at ha hb ⊢, simp at ha hb ⊢,
-      have ha' : a^p^n = a := by rw[←add_left_inj (-a)]; simp only [ha, eq_self_iff_true, add_left_neg],
-      have hb' : b^p^n = b := by rw[←add_left_inj (-b)]; simp only [hb, eq_self_iff_true, add_left_neg],
-      rw[←add_left_inj (a*b), mul_pow],
-      simp only [ha', hb', add_zero, eq_self_iff_true, add_left_neg],
-      repeat {exact h0}
-    end,
-    inv_mem := λ a ha0 ha, begin rw [finset.mem_coe, mem_roots] at ha ⊢, simp at ha ⊢,
-      have ha' : a^p^n = a := by rw[←add_left_inj (-a)]; simp only [ha, eq_self_iff_true, add_left_neg],
-      exact calc -a⁻¹ + a⁻¹^p^n = -(a^p^n)⁻¹ + a⁻¹^p^n  : by rw[ha']
-                            ... = -(a⁻¹^p^n) + a⁻¹^p^n  : sorry
-                            ... = 0                     : by simp only [eq_self_iff_true, add_left_neg],
-      repeat {exact h0}
-    end }
+  { zero_mem := by rw root_Xq_X_iff; exact zero_pow (nat.pow_pos hp.pos n),
+    one_mem  := by rw root_Xq_X_iff; exact one_pow _,
+    add_mem  := λ a b ha hb, by rw root_Xq_X_iff at ha hb ⊢; rw [add_pow_n_char β hp a b, ha, hb],
+    neg_mem  := λ a ha, by rw root_Xq_X_iff at ha ⊢; sorry,
+    mul_mem := λ a b ha hb, by rw root_Xq_X_iff at ha hb ⊢; rw [mul_pow, ha, hb],
+    inv_mem := λ a ha0 ha, by rw root_Xq_X_iff at ha ⊢; sorry }
 
 end finite_field
